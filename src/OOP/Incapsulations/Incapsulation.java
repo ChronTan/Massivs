28a334eab4b5d443fa00def2e7de87f3568ce331
NoName <skarr.age@mail.ru>
2019-07-29 13:42:00 +0300

    OOP.Incap

---
+++ src/OOP/Incapsulations/Incapsulation.java
@@ -0,0 +1,11 @@
+package OOP.Incapsulations;
+
+public class Incapsulation {
+
+    public static void main(String[] args) {
+        Person person=new Person();
+        System.out.println(person.getName());
+        person.setName("Kirill");
+        System.out.println(person.getName());
+    }
+}
---
+++ src/OOP/Incapsulations/Person.java
@@ -0,0 +1,13 @@
+package OOP.Incapsulations;
+
+public class Person {
+    private String name="Misha";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
